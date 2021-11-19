64c54667a090cac84ba120d9b65e423e24fa8cba
unknown <thales.monteiro.moreira@gmail.com>
2021-11-19 16:42:35 -0300

    8 commit

--- js/exibirProdutosTelaInicial.js
+++ js/exibirProdutosTelaInicial.js
@@ -1,40 +1,42 @@
 var url_pedido = 'file:///C:/Users/ravtec/Desktop/ecommerce-master/Mostrar-Produto.html?produto=bola';
-document.querySelector('.comprar1').href = url_pedido;
+document.getElementsByClassName('produto1').href = url_pedido;
 var url_pedido2 = 'file:///C:/Users/ravtec/Desktop/ecommerce-master/Mostrar-Produto.html?produto=camisetawarriors';
-document.querySelector('.comprar2').href = url_pedido2;
+document.getElementsByClassName('produto2').href = url_pedido2;
 var url_pedido3 = 'file:///C:/Users/ravtec/Desktop/ecommerce-master/Mostrar-Produto.html?produto=camisetalakers';
-document.querySelector('.comprar3').href = url_pedido3;
+document.getElementsByClassName('produto3').href = url_pedido3;
 
 
 mostrarProdutoNaTela = (dados) => { 
-    document.getElementById('nome-produto').innerHTML = dados[0].nome
-    document.getElementById('preco-produto').innerHTML = dados[0].preco
+    document.getElementById('nome-produto1').innerHTML = dados[0].nome
+    document.getElementById('preco-produto1').innerHTML = dados[0].preco
     document.getElementById('nome-produto2').innerHTML = dados[0].nome1
     document.getElementById('preco-produto2').innerHTML = dados[0].preco1
     document.getElementById('nome-produto3').innerHTML = dados[0].nome2
     document.getElementById('preco-produto3').innerHTML = dados[0].preco2
 }
-criarDivPrd = () => {
+criarDivPrd = (parametro,num) => {
     var x = document.getElementsByClassName('produto')
     var divNomePrd = document.createElement('div')
-    divNomePrd.setAttribute('class',parametro)
-    divNomePrd.setAttribute('id','sla')
-    x[0].appendChild(divNomePrd)
+    divNomePrd.setAttribute('id', 'nome-'+parametro)
+    divNomePrd.setAttribute('class','nome-produto')
+    x[num].appendChild(divNomePrd)
 
     var y = document.getElementsByClassName('img-produto')
     var img = document.createElement('img')
     img.setAttribute('class','img')
     img.src = "http://localhost:3000/imagens/" +parametro + ".png"
-    y[0].appendChild(img)
+    y[num].appendChild(img)
 
     var divPrecoPrd = document.createElement('div')
     divPrecoPrd.setAttribute('class','preco-produto')
-    divPrecoPrd.setAttribute('id',parametro)
-    x[0].appendChild(divPrecoPrd)
+    divPrecoPrd.setAttribute('id','preco-'+parametro)
+    x[num].appendChild(divPrecoPrd)
 
     var elementoA = document.createElement('a')
     elementoA.setAttribute('class',parametro)
     elementoA.href = ""
+    x[num].appendChild(elementoA)
+    console.log(elementoA);
 }
 
 async function buscarEmostrarProduto() {
@@ -46,8 +48,11 @@ async function buscarEmostrarProduto() {
     const preco1 = dados[0].preco1
     const nome2 = dados[0].nome2
     const preco2 = dados[0].preco2
+    criarDivPrd('produto1','0')
+    criarDivPrd('produto2','1')
+    criarDivPrd('produto3','2')
     mostrarProdutoNaTela(dados)
-    criarDivPrd()
+    
 }
 
 buscarEmostrarProduto();
