4d4c0ccbd1df30a64099d959d1dee757d9146e94
unknown <thales.monteiro.moreira@gmail.com>
2021-11-25 16:51:06 -0300

    mudanças futuras

--- js/exibirProdutosTelaInicial.js
+++ js/exibirProdutosTelaInicial.js
@@ -1,4 +1,4 @@
-
+//for nisso aqui
 mostrarProdutoNaTela = (dados,nomeProduto1,nomeProduto2,nomeProduto3) => { 
     document.getElementById('nome-'+nomeProduto1).innerHTML = dados[0].nome
     document.getElementById('preco-'+nomeProduto1).innerHTML = dados[0].preco
@@ -8,27 +8,27 @@ mostrarProdutoNaTela = (dados,nomeProduto1,nomeProduto2,nomeProduto3) => {
     document.getElementById('preco-'+nomeProduto3).innerHTML = dados[0].preco2
 }
 criarDivPrd = (nomeProduto,num,nomeUrl) => {
-    var x = document.getElementsByClassName('produto')
+    var divPrd = document.getElementsByClassName('produto')
     var divNomePrd = document.createElement('div')
     divNomePrd.setAttribute('id', 'nome-'+nomeProduto)
     divNomePrd.setAttribute('class','nome-produto')
-    x[num].appendChild(divNomePrd)
+    divPrd[num].appendChild(divNomePrd)
 
     var img = document.createElement('img')
     img.setAttribute('class','img')
     img.src = "http://localhost:3000/imagens/" +nomeProduto + ".png"
-    x[num].appendChild(img)
+    divPrd[num].appendChild(img)
 
     var divPrecoPrd = document.createElement('div')
     divPrecoPrd.setAttribute('class','preco-produto')
     divPrecoPrd.setAttribute('id','preco-'+nomeProduto)
-    x[num].appendChild(divPrecoPrd)
+    divPrd[num].appendChild(divPrecoPrd)
 
     var elementoA = document.createElement('a')
     elementoA.setAttribute('class',nomeProduto)
     elementoA.setAttribute('href', '../ecommerce-master/Mostrar-Produto.html?produto='+nomeUrl)
     elementoA.innerText = 'comprar'
-    x[num].appendChild(elementoA)
+    divPrd[num].appendChild(elementoA)
 }
 
 async function buscarEmostrarProduto() {
